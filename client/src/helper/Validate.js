70277762e4d11f486e8905b9bc7590dfed1f45fd
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-03-12 23:16:45 +0530

    create and validate password recovery and reset password UI

--- client/src/helper/Validate.js
+++ client/src/helper/Validate.js
@@ -7,12 +7,50 @@ export const usernameValidate = (values) => {
   return errors;
 };
 
+/**Validate login page password */
+export const passwordValidate = (values) => {
+  const errors = passwordVerify({}, values);
+
+  return errors;
+};
+
+/**Validate reset password */
+export const resetPasswordValidate = (values) => {
+  const errors = passwordVerify({}, values);
+
+  if (values.password !== values.confirmPassword) {
+    errors.username = toast.error("Password not match...!");
+  }
+  return errors;
+};
+
+/************************************************************** */
+
 /**Validate username */
 const usernameVerify = (error = {}, values) => {
   if (!values.username) {
-    error.username = toast.error("Username Required");
+    error.username = toast.error("Username Required...!");
   } else if (values.username.includes(" ")) {
-    error.username = toast.error("Invalid username");
+    error.username = toast.error("Invalid username...!");
+  }
+  return error;
+};
+
+/**Validate username */
+const passwordVerify = (error = {}, values) => {
+  // eslint-disable-next-line no-useless-escape
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+  if (!values.password) {
+    error.password = toast.error("password Required...!");
+  } else if (values.password.includes(" ")) {
+    error.username = toast.error("Wrong password...!");
+  } else if (values.password.length < 4) {
+    error.username = toast.error(
+      "Password must be more than 4 characters long"
+    );
+  } else if (!specialChars.test(values.password)) {
+    error.username = toast.error("Password must have special characters");
   }
   return error;
 };
